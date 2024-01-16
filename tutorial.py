e735b3e8d79bdadd289460b358b6961818c1ccd9
Kartik Saxena <karrtiksaxena@gmail.com>
2024-01-17 01:49:13 +0530

    selenium starts

---
+++ tutorial.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.by import By
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome()
+driver.get("https://google.com")
+print(driver.title)
+element = driver.find_element(By.CSS_SELECTOR, "textarea.gLFyf")
+element.send_keys("hi mom")
+element.send_keys(Keys.RETURN)
+time.sleep(10)
