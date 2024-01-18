df593a518528e79cf23e15adba51e3dfde3161fa
Kartik Saxena <karrtiksaxena@gmail.com>
2024-01-19 02:01:55 +0530

    webDriverSupport

--- tutorial.py
+++ tutorial.py
@@ -8,6 +8,9 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome()
 driver.get("https://google.com")
 print(driver.title)
+WebDriverWait(driver,5).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "textarea.gLFyf"))
+)
 element = driver.find_element(By.CSS_SELECTOR, "textarea.gLFyf")
 element.send_keys("hi mom"+Keys.RETURN)
 movie=driver.find_element(By.CLASS_NAME, "EKtkFWMYpwzMKOYr0GYm LQVY1Jpkk8nyJ6HBWKAk").click()
