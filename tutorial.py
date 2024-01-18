5cdca4c144e5d9ef287d27a8e1ab8cac42fc5e8b
Kartik Saxena <karrtiksaxena@gmail.com>
2024-01-19 01:59:30 +0530

    nav

--- tutorial.py
+++ tutorial.py
@@ -2,10 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome()
 driver.get("https://google.com")
 print(driver.title)
 element = driver.find_element(By.CSS_SELECTOR, "textarea.gLFyf")
 element.send_keys("hi mom"+Keys.RETURN)
+movie=driver.find_element(By.CLASS_NAME, "EKtkFWMYpwzMKOYr0GYm LQVY1Jpkk8nyJ6HBWKAk").click()
+movie2=driver.find_element(By.CSS_SELECTOR, "video.jw-video jw-reset").click()
+
 time.sleep(10)
